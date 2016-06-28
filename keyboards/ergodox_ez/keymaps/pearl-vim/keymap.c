4d009882f76d637f2a1ab5a026da064acf34db12
Mark Pearl <markpearl@gmail.com>
2016-06-28 06:26:53 +0200

    More adjustments

--- keyboards/ergodox_ez/keymaps/pearl-vim/keymap.c
+++ keyboards/ergodox_ez/keymaps/pearl-vim/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |----------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift   |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `----------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |LGUI-L2 |  '"  |   /  | Left | Right|                                       |  Up  | Down |   [  |   ]  | RGUI |
+ *   |LGUI-L2 |   ~  |   /  | PRNT |      |                                       |  INS | PAUS |   [  |   ]  | RGUI |
  *   `------------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |LCTRL | LALT |       | RAlt |RCTRL|
@@ -38,23 +38,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,            KC_Q,     KC_W,        KC_E,     KC_R,   KC_T,   TG(VIM),
         KC_ESC,            KC_A,     KC_S,        KC_D,     KC_F,   KC_G,
         KC_LSFT,           KC_Z,     KC_X,        KC_C,     KC_V,   KC_B,   ALL_T(KC_NO),
-        LT(MDIA, KC_LGUI), KC_QUOT,  KC_SLASH,    KC_LEFT,  KC_RGHT,
+        LT(MDIA, KC_LGUI), KC_GRAVE, KC_SLASH,    KC_PSCR,  KC_NO,
 
 	// left thumb area
-                                             KC_LCTRL, KC_LALT,
-                                                       KC_HOME,
-                                              KC_BSPC, KC_DELETE, KC_END,
+        KC_LCTRL, KC_LALT,
+        KC_HOME,
+        KC_BSPC, KC_DELETE, KC_END,
+
         // right hand
-            KC_RGHT, KC_6,   KC_7,   KC_8,    KC_9,              KC_0,     KC_MINS,
-           OSL(VIM), KC_Y,   KC_U,   KC_I,    KC_O,              KC_P,     KC_BSLS,
-               KC_H, KC_J,   KC_K,   KC_L,    KC_SCLN,           KC_QUOT,
-       MEH_T(KC_NO), KC_N,   KC_M,   KC_COMM, KC_DOT,            KC_SLSH,  KC_RSFT,
-                             KC_UP,  KC_DOWN, KC_LBRC,           KC_RBRC,  KC_RGUI,
+            KC_RGHT, KC_6,   KC_7,   KC_8,     KC_9,              KC_0,     KC_MINS,
+           OSL(VIM), KC_Y,   KC_U,   KC_I,     KC_O,              KC_P,     KC_BSLS,
+               KC_H, KC_J,   KC_K,   KC_L,     KC_SCLN,           KC_QUOT,
+       MEH_T(KC_NO), KC_N,   KC_M,   KC_COMM,  KC_DOT,            KC_SLSH,  KC_RSFT,
+                             KC_INS, KC_PAUSE, KC_LBRC,           KC_RBRC,  KC_RGUI,
 
 	// right thumb area
-             KC_RALT, KC_RCTRL,
-             KC_PGUP,
-             KC_PGDN, KC_ENT, KC_SPACE
+        KC_RALT, KC_RCTRL,
+        KC_PGUP,
+        KC_PGDN, KC_ENT, KC_SPACE
     ),
 /* Keymap 1: Symbol Layer
  *
@@ -65,44 +66,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | L1     |      |      |      |      |      |------|           |------| left | down |  up  | right|      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |      | DEL  |      |      |      |      |           |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |      |      |       |      |      |
- *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
- *                                 |      |      |      |       |      |      |      |
- *                                 `--------------------'       `--------------------'
+ *                                        |LCTRL | LALT |       | RAlt |RCTRL|
+ *                                 ,------|------|------|       |------+--------+------.
+ *                                 |      |      | Home |       | PgUp |        |      |
+ *                                 | BKSP |  DEL |------|       |------|  Enter |Space |
+ *                                 |      |	 | End  |       | PgDn |        |      |
+ *                                 `--------------------'       `----------------------'
  */
 // VIMOLS
 [VIM] = KEYMAP(
 
        // left hand
-       KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       TG(VIM), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_NO  , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO  ,
+       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,
+       TG(VIM), KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,
+       KC_NO  , KC_NO  , KC_DEL , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,
+       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,
 
 	// left thumb area
-	   	  KC_LCTRL, KC_LALT,
-		    	    KC_HOME,
-         KC_BSPC, KC_DELETE, KC_END,
+	KC_LCTRL, KC_LALT,
+        KC_HOME,
+        KC_BSPC, KC_DELETE, KC_END,
 
        // right hand
-       KC_F12,  KC_F6,   KC_F7,   KC_F8,    KC_F9,    KC_F10,  KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, TG(VIM),  KC_TRNS,  KC_TRNS, KC_TRNS,
-                KC_LEFT, KC_DOWN, KC_UP,    KC_RIGHT, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,
-                         KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,
+       KC_F12,  KC_F6,   KC_F7,   KC_F8,    KC_F9,    KC_F10,  KC_NO  ,
+       KC_NO  , KC_NO  , KC_NO,   TG(VIM),  KC_NO,    KC_NO  , KC_NO  ,
+                KC_LEFT, KC_DOWN, KC_UP,    KC_RIGHT, KC_NO  , KC_NO  ,
+       KC_NO  , KC_NO  , KC_NO  , KC_NO  ,  KC_NO  ,  KC_NO  , KC_NO  ,
+                         KC_NO  , KC_NO  ,  KC_NO  ,  KC_NO  , KC_NO  ,
 
 	// right thumb area
-             KC_RALT, KC_RCTRL,
-             KC_PGUP,
-             KC_PGDN, KC_ENT, KC_SPACE
+        KC_RALT, KC_RCTRL,
+        KC_PGUP,
+        KC_PGDN, KC_ENT, KC_SPACE
 ),
 
 /* Keymap 2: Media and mouse keys
@@ -133,15 +134,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                           KC_TRNS, KC_TRNS,
-                                                    KC_TRNS,
-                                  KC_TRNS, KC_TRNS, KC_TRNS,
-    // right hand
+
+	// left thumb area
+       KC_TRNS, KC_TRNS,
+       KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS,
+
+       // right hand
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS, KC_TRNS,
        KC_TRNS,  KC_BTN1, KC_TRNS, KC_TRNS, KC_BTN2, KC_TRNS, KC_TRNS,
                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+
+       // right thumb area
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_WBAK
@@ -181,6 +187,7 @@ void matrix_scan_user(void) {
     ergodox_right_led_1_off();
     ergodox_right_led_2_off();
     ergodox_right_led_3_off();
+
     switch (layer) {
       // TODO: Make this relevant to the ErgoDox EZ.
         case 1:
